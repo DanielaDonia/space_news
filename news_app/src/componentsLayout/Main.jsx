9b0b20abf3cd7025dfcb9ad0ccdad8bfaf54c853
Daniela <Daniela Gomes@LAPTOP-194QUC87.(none)>
2024-03-18 13:35:50 +0000

    searchButton

--- news_app/src/componentsLayout/Main.jsx
+++ news_app/src/componentsLayout/Main.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
-//import useLocalStorage from 'use-local-storage';
 import { useTheme, useThemeUpdate} from './LightDarkTheme';
 
 const Main = ({fetchData}) => {
     const [data, setData] = useState(null);
     const [orderBy, setOrderBy] = useState('');
-    const [pagination, setPagination] = useState('published_at');
-const [value, setValue] = useState('');
-//const [isDark, setIsDark] = useLocalStorage('isDark', false);
-const [loading, setLoading] = useState(false);
-const darkTheme = useTheme();
+    const [pagination, setPagination] = useState('published_at');   
+    const [loading, setLoading] = useState(false);
+    const[error, setError] = useState(false);
+    const darkTheme = useTheme();
     const toggleTheme = useThemeUpdate();
+    const [searchValue, setSearchValue] = useState('');
+    const [originalData, setOriginalData] = useState(null); 
+
   
 
 useEffect(() => {
@@ -22,14 +23,13 @@ useEffect(() => {
             setLoading(true);
             const api = await fetchData(pagination,orderBy);
             if (!api.data) {
-                console.error('no API data', api);
+                setError(true)
                 return;
             }
-            console.log(api.data, 'render useEffect');
             setData(api.data);
-            setLoading(false);
+            setOriginalData(api.data);
         } catch (error) {
-            console.error('Error api', error);
+            setError(true)
         }finally{
             setLoading(false);
         }
@@ -50,8 +50,6 @@ useEffect(() => {
            setPagination(data.next);
          
         }
-       
-
     }
 
     const handlerPrevious = ()=>{
@@ -62,72 +60,80 @@ useEffect(() => {
         }
     }
 
-const handlerSearch = async()=>{
-    try{
-        const searchResults = await fetchData('search', value, setLoading);
-    setData(searchResults.data)
-    console.log(searchResults.data,'search')
-    }catch(error){
-        console.log(error);
-    }
-
-}
-
-
-
-const handlerInput = (e)=>{
-    setValue(e.target.value);
-
+    
+    const handlerSearch = () => {
+        if (!searchValue.trim()) {
+            setData(originalData); 
+            return;
+        }
+        const filteredData = originalData.results.filter(item =>
+            item.title.toLowerCase().includes(searchValue.toLowerCase())
+        );
+        if(filteredData.length === 0){
+            setError(true);
+        }else{
+ setData({ ...originalData, results: filteredData });
+ setError(false)
+        }
+       
 }
     
 
 
+const handlerInput = (e)=>{
+    setSearchValue(e.target.value);
 
+}
+    
 const toggleDarkMode = () => {
       toggleTheme();
     };
 
     return (
-      
-
- <main className={darkTheme ? 'dark-mode' : 'light-mode'}>
-        <button id="darkButton" onClick={toggleDarkMode}>Dark Mode</button>          
-<div id='mainContainer' >
-            <div id="searchContainer"> 
-            <input 
-        type="text"
-        placeholder="Search Here"
-    value={value}
-    onChange={handlerInput}
-    className='search-input'>
-        </input>
-        <button id='button-ok'onClick={handlerSearch}>Ok</button>
-        </div>
-            <div>
-                <label id="order">Order By:</label>
-                <select value={orderBy} onChange={handlerOrder}>
-                    <option value='-published_at'>Recent</option>
-                    <option value='published_at'>Older</option>
-                </select>
+        <main className={darkTheme ? 'dark-mode' : 'light-mode'}>
+        <button id="darkButton" onClick={toggleDarkMode}>Dark Mode</button>
+        <div id='mainContainer'>
+            <div id="searchContainer">
+                <input
+                    type="text"
+                    placeholder="Search Here"
+                    value={searchValue}
+                    onChange={handlerInput}
+                    className='search-input'
+                />
+                <button id='button-ok' onClick={handlerSearch}>Ok</button>
             </div>
-            <div className="buttonHandler">
-                <button id='previous' value={pagination} onClick={handlerPrevious}>Previous</button>
-            <button id='next' value={pagination} onClick={handlerNext}>Next</button>
-            </div>
-            
-            {loading && <img id='loading'src='https://i.pinimg.com/originals/bd/b6/76/bdb676999f2be7140bc96a4e50a584b8.gif' alt="Loading" />}
-
-            <div id='newsContainer'>
-                {data && data.results.map(item => (
-                    <div key={item.id}>
-                        <h3>{item.title}</h3>
-                        <img src={item.image_url} alt={item.title}/>
-                        <br>
-                        </br>
-                        <a id='site'href={item.url}>{item.url}</a>
+            {!error && !loading && (
+                <>
+                    <div>
+                        <label id="order">Order By:</label>
+                        <select value={orderBy} onChange={handlerOrder}>
+                            <option value='-published_at'>Recent</option>
+                            <option value='published_at'>Older</option>
+                        </select>
                     </div>
-                ))}
-            </div>
+                    <div className="buttonHandler">
+                        <button id='previous' value={pagination} onClick={handlerPrevious}>Previous</button>
+                        <button id='next' value={pagination} onClick={handlerNext}>Next</button>
+                    </div>
+                </>
+            )}
+            {loading && <img id='loading' src='https://i.pinimg.com/originals/bd/b6/76/bdb676999f2be7140bc96a4e50a584b8.gif' alt="Loading" />}
+            {error && !loading && (
+                <img id="error" src="https://media.tenor.com/CG0fYi2zdkEAAAAM/lost-in-space-retro-tv.gif" alt="error" />
+            )}
+            {!error && data && (
+                <div id='newsContainer'>
+                    {data.results.map(item => (
+                        <div key={item.id}>
+                            <h3>{item.title}</h3>
+                            <img src={item.image_url} alt={item.title} />
+                            <br />
+                            <a id='site' href={item.url}>{item.url}</a>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     </main> 
 
